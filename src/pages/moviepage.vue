90c84d2608b8c132b35c0bf8968c0c9752227982
josaks <josaks@online.no>
2018-10-14 16:05:20 +0200

    Move movie computed property arguments from template to script

--- src/pages/moviepage.vue
+++ src/pages/moviepage.vue
@@ -1,12 +1,12 @@
 <template>
     <div>
-        <div>{{ movie(parseInt(id)).title }}</div>
+        <div>{{ movie.title }}</div>
         <form v-on:submit.prevent="addComment()">
             <input v-model="text" type="text" name="text" vale="" placeholder="Write a comment">
             <button type="submit" name="Submit">Submit</button>
         </form>
         <ul>
-            <li v-for="comment in movie(parseInt(id)).comments" :key="comment.text">{{ comment.text }}</li>
+            <li v-for="comment in movie.comments" :key="comment.text">{{ comment.text }}</li>
         </ul>
     </div>
 </template>
@@ -21,9 +21,11 @@ export default {
     props: ['id'],
     computed: {
         ...mapGetters({
-            movie: 'movie',
             comments: 'comments',
         }),
+        movie: function() {
+            return this.$store.getters['movie'](parseInt(this.id));
+        },
     },
     methods: {
         ...mapActions({
