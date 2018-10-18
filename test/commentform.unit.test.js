6135dbb4561cb284da89f6b0764cd1c8ef18da28
josaks <josaks@online.no>
2018-10-18 13:31:23 +0200

    Update commentform test

--- test/commentform.unit.test.js
+++ test/commentform.unit.test.js
@@ -1,25 +1,29 @@
 import CommentForm from "../src/components/commentform";
-import { shallowMount } from "@vue/test-utils";
 
 
 describe("Comment form", () => {
-    const commentForm = shallowMount(CommentForm, {
-        methods: {
-            addComment() {}
-        }
-    });
-    
+    const mockMethod = jest.fn();
+    const commentForm = mount(CommentForm);
+    commentForm.setMethods({ submit: mockMethod });
 
     it("renders a vue instance", () => {
         expect(commentForm.isVueInstance()).toBeTruthy();
     });
 
     it("renders a form", () => {
-        expect(commentForm.contains("vform-stub")).toBeTruthy();
+        expect(commentForm.contains(".v-form")).toBeTruthy();
     });
 
     it("renders a button", () => {
-        const btn = commentForm.find("vbtn-stub");
+        const btn = commentForm.find(".v-btn");
         expect(btn.exists()).toBeTruthy();
+    });
+
+    describe("button", () => {
+        it("calls correct method when clicked", () => {
+            const btn = commentForm.find(".v-btn");
+            btn.trigger("click");
+            expect(mockMethod).toBeCalled();
+        });
     });
 });
